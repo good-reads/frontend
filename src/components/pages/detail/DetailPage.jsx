587283dd2b25cc81ac7b53ecb4448c09dc9f732d
snaag <04_miffy@naver.com>
2020-09-10 00:22:35 +0900

    :sparkles: DetailPage 책 정보 불러오도록 구현

--- src/components/pages/detail/DetailPage.jsx
+++ src/components/pages/detail/DetailPage.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
-const DetailPage = () => {
-  return <div>DetailPage</div>;
+import BookDetail from './components/BookDetail';
+
+const DetailPage = ({ history }) => {
+  // const { isbn } = history.location.state;
+
+  return (
+    <div>
+      <BookDetail info={history.location.state} />
+    </div>
+  );
 };
 
-export default DetailPage;
+export default withRouter(DetailPage);
