3d4974ac4f9596d1a7921d739549d7714f7b21ad
snaag <04_miffy@naver.com>
2020-09-28 01:08:41 +0900

    :shirt: 아이콘들 분리함

---
+++ src/components/icons/Icons.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+export const LoadingIcon = () => {
+  return <FontAwesomeIcon icon={['fas', 'spinner']} spin />;
+};
+
+export const SearchIcon = () => {
+  return <FontAwesomeIcon icon={['fas', 'search']} />;
+};
+
+export const CloseIcon = () => {
+  return <FontAwesomeIcon icon={['fas', 'times']} />;
+};
+
+export const SignOutIcon = () => {
+  return <FontAwesomeIcon icon={['fas', 'sign-out-alt']} />;
+};
+
+export const SignInIcon = () => {
+  return <FontAwesomeIcon icon={['fas', 'sign-in-alt']} />;
+};
+
+export const SignUpIcon = () => {
+  return <FontAwesomeIcon icon={['fas', 'user-plus']} />;
+};
