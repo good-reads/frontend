7f9177fe72f6c1e6a29d6e69e18215cf4aeda2c3
snaag <04_miffy@naver.com>
2020-10-03 03:20:33 +0900

    :shirt: ListPage SCSS 적용

--- src/components/pages/list/components/Book.jsx
+++ src/components/pages/list/components/Book.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 
+import * as Icons from '../../../icons/Icons';
+
 const Book = ({ info, deleteBook }) => {
   const { isbn, title, author, rate, cover } = info;
 
   return (
-    <div>
-      <img src={cover} alt={title} />
-      <span>{title}</span>
-      <span>{author}</span>
-      <button onClick={() => deleteBook(isbn)}>X</button>
+    <div className="book-info">
+      <div className="book-info__inner">
+        <button className="book-info__remove" onClick={() => deleteBook(isbn)}>
+          <Icons.CloseIcon />
+        </button>
+        <div className="book-info__inner-image">
+          <img className="book-info__image" src={cover} alt={title} />
+        </div>
+
+        <div className="book-info__inner-detail">
+          <span className="book-info__item book-info__title">{title}</span>
+          <span className="book-info__item book-info__author">{author}</span>
+        </div>
+      </div>
     </div>
   );
 };
